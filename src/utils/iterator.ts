0a41891b16e4497a64c7b7be856cc0e2c83d201e
Gonzalo Ruiz <rgon@rgon.es>
2025-11-24 22:14:04 +0100

    fix: processing PAPacket hangs if no tags returned from PA server #41
    Co-authored-by: Vyacheslav <skyliver.ik@gmail.com>

--- src/utils/iterator.ts
+++ src/utils/iterator.ts
@@ -17,7 +17,7 @@ export function createIterator (array: any[]): Iterator<any> {
       if (++this.index === this.length) this.done = true
       return iterator.next().value
     },
-    done: false,
+    done: array.length === 0,
     index: 0,
     length: array.length
   }
